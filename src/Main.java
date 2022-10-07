764d28e7d4ebb39b2e2ea25637c6ed80233fb0a0
lukaa <luka.andre0112@gmail.com>
2022-10-07 11:21:53 +0300

    Сommit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int moneyYear = 0; // годовой доход
         Scanner write = new Scanner(System.in);
-
         System.out.print("Введите доход за год ");
         moneyYear = Integer.parseInt(write.nextLine());
         calcMoney(moneyYear);
@@ -12,10 +11,10 @@ public class Main {
     }
 
 
-    private static void calcMoney(int moneyYear) { //Метод для вычесления отпускных
-        int DAYSFREE = 28;  // Колличество дней в отпууске
+    private static void calcMoney(int moneyYear) {   //Метод для вычесления отпускных
+        int DAYSFREE = 28;  // Колличество дней в отпуске
         double MONTH = 12;
-        double DAYS = 29.5;   //Коллличество дней утановленых законом
+        double DAYS = 29.5;   //Коллличество дней усстановленых законом
         double calc = (moneyYear / MONTH / DAYS)* DAYSFREE;
         String string = String.format ("%.0f", calc);
         System.out.print(string);
